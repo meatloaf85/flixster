9280ccabdd4e66fe7ec8a40aa4724fc80ea61dd1
jar <T@FPg4G!iFfK!e66>
2021-02-26 13:02:58 -0700

    thank goodness

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by jar on 2/24/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+
+}
